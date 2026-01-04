b5251a35e839321352dd9d26786deee1b4c35b1f
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2026-01-04 21:37:44 +0200

    fixing the issue of redeploying

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss(),
+
+
+    
+  ],
+
+   build: {
+    outDir: 'build', // so Render can find the folder
+  },
+
+  
 })
